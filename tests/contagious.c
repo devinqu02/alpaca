3886137eb62a72532ed9f204a58ca0784e6d0afb
Devin Qu <devinqu02@gmail.com>
2024-12-01 20:22:33 -0500

    add test for contagious privatization

---
+++ tests/contagious.c
@@ -0,0 +1,27 @@
+#include "alpaca_tasks.h"
+
+#include <stdio.h>
+
+NV_GLOBAL int a, b;
+
+void f() {
+    printf("%d %d\n", a, b);
+}
+
+TASK(task2) {
+    ++b;
+    f();
+    TRANSITION_TO(end);
+}
+
+TASK(task1) {
+    ++a;
+    f();
+    TRANSITION_TO(task2);
+}
+
+MAIN_TASK {
+    JUMP_TO(task1);
+}
+
+INITIALIZATION {}
